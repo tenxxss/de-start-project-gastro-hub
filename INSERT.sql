6c16cd9be9a9ef70f92b37df8444a75c1e115d46
tenxxss <56553912+tenxxss@users.noreply.github.com>
2026-09-20 17:17:11 +0300

    INSERT.sql

--- INSERT.sql
+++ INSERT.sql
@@ -1,15 +1,12 @@
--- 1. Рестораны
 INSERT INTO cafe.restaurants (cafe_name, type, menu)
 SELECT s.cafe_name, s.type::cafe.restaurant_type, m.menu
 FROM (SELECT DISTINCT cafe_name, type FROM raw_data.sales) s
 LEFT JOIN raw_data.menu m USING (cafe_name);
 
--- 2. Менеджеры
 INSERT INTO cafe.managers (manager, manager_phone)
 SELECT DISTINCT manager, manager_phone
 FROM raw_data.sales;
 
--- 3. Периоды работы менеджеров в ресторанах
 INSERT INTO cafe.restaurant_manager_work_dates (restaurant_uuid, manager_uuid, start_date, end_date)
 SELECT r.restaurant_uuid, m.manager_uuid, MIN(s.report_date), MAX(s.report_date)
 FROM raw_data.sales s
@@ -17,7 +14,6 @@ JOIN cafe.restaurants r ON r.cafe_name = s.cafe_name
 JOIN cafe.managers m ON m.manager = s.manager
 GROUP BY r.restaurant_uuid, m.manager_uuid;
 
--- 4. Продажи
 INSERT INTO cafe.sales (date, restaurant_uuid, avg_check)
 SELECT s.report_date, r.restaurant_uuid, s.avg_check
 FROM raw_data.sales s
