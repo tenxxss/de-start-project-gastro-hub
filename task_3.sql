e8269aa89dd854c100596be2d202a27be5e84740
tenxxss <56553912+tenxxss@users.noreply.github.com>
2026-09-20 16:41:18 +0300

    task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,1 +1,8 @@
-/*добавьте сюда запрос для решения задания 3*/
+SELECT
+    r.cafe_name,
+    COUNT(*) AS manager_changes
+FROM cafe.restaurant_manager_work_dates w
+JOIN cafe.restaurants r USING (restaurant_uuid)
+GROUP BY r.restaurant_uuid, r.cafe_name
+ORDER BY manager_changes DESC
+LIMIT 3;
