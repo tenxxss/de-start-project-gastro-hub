9290fb74090fc5623f6d4c685e443466ed648128
tenxxss <56553912+tenxxss@users.noreply.github.com>
2026-09-20 16:43:44 +0300

    task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,1 +1,11 @@
-/*добавьте сюда запрос для решения задания 4*/
+WITH pizza_counts AS (
+    SELECT
+        cafe_name,
+        (SELECT COUNT(*) FROM jsonb_object_keys(menu -> 'Пицца')) AS pizza_count
+    FROM cafe.restaurants
+    WHERE type = 'pizzeria'
+)
+SELECT cafe_name, pizza_count
+FROM pizza_counts
+WHERE pizza_count = (SELECT MAX(pizza_count) FROM pizza_counts)
+ORDER BY cafe_name;
